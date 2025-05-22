9acd27e184a43b69a07877bd26e0ccc9d0b6fbaf
UHeeJoon <yhjyhj3855@gmail.com>
2025-05-23 01:29:27 +0900

    [#1]feat: 포인트 조회, 충전, 사용, 충전/사용 내역 조회 컨트롤러 작성

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,25 +1,29 @@
 package io.hhplus.tdd.point;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/point")
+@RequiredArgsConstructor
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService pointService;
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}")
     public UserPoint point(
-            @PathVariable long id
+      @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        return loggingMethodTime(() -> pointService.retrieveUserPointByUserId(id));
     }
 
     /**
@@ -27,9 +31,9 @@ public class PointController {
      */
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
-            @PathVariable long id
+      @PathVariable long id
     ) {
-        return List.of();
+        return loggingMethodTime(() -> pointService.retrievePointHistoryByUserId(id));
     }
 
     /**
@@ -37,10 +41,10 @@ public class PointController {
      */
     @PatchMapping("{id}/charge")
     public UserPoint charge(
-            @PathVariable long id,
-            @RequestBody long amount
+      @PathVariable long id,
+      @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return loggingMethodTime(() -> pointService.chargePoint(id, amount));
     }
 
     /**
@@ -48,9 +52,20 @@ public class PointController {
      */
     @PatchMapping("{id}/use")
     public UserPoint use(
-            @PathVariable long id,
-            @RequestBody long amount
+      @PathVariable long id,
+      @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return loggingMethodTime(() -> pointService.usePoint(id, amount));
+    }
+
+    /**
+     * 메서드 실행 시간 로깅 - 간단 구현
+     */
+    private <T> T loggingMethodTime(final Supplier<T> method) {
+        final long startTime = System.currentTimeMillis();
+        final T methodRunningResult = method.get();
+        final long endTime = System.currentTimeMillis();
+        log.info("method execute time : {}ms", endTime - startTime);
+        return methodRunningResult;
     }
 }
